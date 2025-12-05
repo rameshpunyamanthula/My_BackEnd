c11b56920212de386b7eb8240a32501ec0535227
Ramesh9689 <punyamanthularamesh53@gmail.com>
2025-12-05 11:10:20 +0530

    Server Creation

---
+++ Index.js
@@ -0,0 +1,18 @@
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+    console.log(req.method);
+    if(req.url=="/users"){
+         res.writeHead(200, { "content-type": "application/json" });
+         res.end(JSON.stringify({Name: "Thub"}))
+    }else
+    res.writeHead(200, { "content-type": "text/plain" });
+    res.end("Hello this is from backend...");
+});
+
+server.listen(7007, () => {
+    console.log(`Server running at port ${7007}`);
+});
+
+
+
