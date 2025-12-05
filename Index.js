3e435937efc2480fbd57b0df81522fae2c35b638
Ramesh9689 <punyamanthularamesh53@gmail.com>
2025-12-05 11:48:02 +0530

    Server creation with Express

--- Index.js
+++ Index.js
@@ -1,18 +1,37 @@
-import http from 'http';
 
-const server = http.createServer((req, res) => {
-    console.log(req.method);
-    if(req.url=="/users"){
-         res.writeHead(200, { "content-type": "application/json" });
-         res.end(JSON.stringify({Name: "Thub"}))
-    }else
-    res.writeHead(200, { "content-type": "text/plain" });
-    res.end("Hello this is from backend...");
-});
+// import http from 'http';
 
-server.listen(7007, () => {
-    console.log(`Server running at port ${7007}`);
-});
+// const server = http.createServer((req, res) => {
+//     console.log(req.method);
+//     if(req.url=="/users"){
+//          res.writeHead(200, { "content-type": "application/json" });
+//          res.send(JSON.stringify({Name: "Thub"}))
+//     }else
+//     res.writeHead(200, { "content-type": "text/plain" });
+//     res.end("Hello this is from backend...");
+// });
+
+// server.listen(7007, () => {
+//     console.log(`Server running at port ${7007}`);
+// });
+
+
+import express from 'express';
+const app = express();
+
+app.get('/users',(req,res) => {
+    res.send("Hello This is from Backend using Express");
+})
+
+app.get('/get-users',(req,res) =>{
+    res.send("Hello this is get users endpoint");
+})
+
+app.listen(7007,() => {
+    console.log("Server is running at port 7007");
+})
+
+
 
 
 
