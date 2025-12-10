96a27b215449bf9c0314de921bd6eab395763d2a
Ramesh9689 <punyamanthularamesh53@gmail.com>
2025-12-10 11:33:31 +0530

    routes and controlles are used

--- Index.js
+++ Index.js
@@ -1,23 +1,28 @@
+/*
+import http from 'http';
 
-// import http from 'http';
+const server = http.createServer((req, res) => {
+    console.log(req.method);
+    if(req.url=="/users"){
+         res.writeHead(200, { "content-type": "application/json" });
+         res.send(JSON.stringify({Name: "Thub"}))
+    }else
+    res.writeHead(200, { "content-type": "text/plain" });
+    res.end("Hello this is from backend...");
+});
 
-// const server = http.createServer((req, res) => {
-//     console.log(req.method);
-//     if(req.url=="/users"){
-//          res.writeHead(200, { "content-type": "application/json" });
-//          res.send(JSON.stringify({Name: "Thub"}))
-//     }else
-//     res.writeHead(200, { "content-type": "text/plain" });
-//     res.end("Hello this is from backend...");
-// });
-
-// server.listen(7007, () => {
-//     console.log(`Server running at port ${7007}`);
-// });
-
+server.listen(7007, () => {
+    console.log(`Server running at port ${7007}`);
+});
+*/
+//---------------------------------------------------------------------------------------
 
 import express from 'express';
+import StudentRouter from './routes/studentRoutes.js';
 const app = express();
+
+app.use('/', StudentRouter);
+
 
 app.get('/users',(req,res) => {
     res.send("Hello This is from Backend using Express");
