f34584a546d63aea6377c82c0adfe1a02ac6238b
Ramesh9689 <punyamanthularamesh53@gmail.com>
2025-12-12 11:52:14 +0530

    Server Connection established

--- Index.js
+++ Index.js
@@ -1,42 +1,62 @@
-/*
-import http from 'http';
-
-const server = http.createServer((req, res) => {
-    console.log(req.method);
-    if(req.url=="/users"){
-         res.writeHead(200, { "content-type": "application/json" });
-         res.send(JSON.stringify({Name: "Thub"}))
-    }else
-    res.writeHead(200, { "content-type": "text/plain" });
-    res.end("Hello this is from backend...");
-});
-
-server.listen(7007, () => {
-    console.log(`Server running at port ${7007}`);
-});
-*/
-//---------------------------------------------------------------------------------------
-
 import express from 'express';
 import StudentRouter from './routes/studentRoutes.js';
+import cors from 'cors';
 const app = express();
 
 app.use('/', StudentRouter);
-
-
-app.get('/users',(req,res) => {
-    res.send("Hello This is from Backend using Express");
-})
+app.use(cors());
 
 app.get('/get-users',(req,res) =>{
     res.send("Hello this is get users endpoint");
+})
+
+app.post('/add-users',(req,res) =>{
+    console.log(req.body);
+    res.send("Data Added successfully");
 })
 
 app.listen(7007,() => {
     console.log("Server is running at port 7007");
 })
 
+// Frontend code
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import axios from 'axios'
+// import './App.css'
+
+// function App() {
+
+//   let baseURL = "http://localhost:7007";
+
+//   const getData = ()=>{
+//     axios.get(`${baseURL}/get-users`)
+//     .then((result) => console.log(result.data))
+//     .catch((error) => console.log(error));
+//   }
 
 
+//   const addData = () =>{
+//     let userData = {
+//       name:"Thub",
+//       rollno : "1234"
+//     };
+//     axios.post(`${baseURL}/add-users`,userData)
+    
+    
+//     .then((result) => console.log(result.data))
+//     .catch((error) => console.log(error));
+//      console.log(userData);
+//   }
 
+//   return (
+//     <>
+//       <button onClick={getData}>Fetch Data</button>
+//       <button onClick={addData}>Add Data</button>
 
+//     </>
+//   )
+// }
+
+// export default App
